e3f6ff3ad4ddd439d750a335ab6bf86e73142523
Nettosh-f <netta.bensinai@gmail.com>
2024-12-04 18:02:43 +0200

    improved ai response

--- AI/gemini.mjs
+++ AI/gemini.mjs
@@ -9,21 +9,21 @@ const userData = {
   Height: 176,
   Weight: 87,
   Background: "basketball 1 time a week for 2 hours",
-  Unit: "669",
+  Unit: "golani",
   Frequency: "3 days",
   Profile: 42,
   Gym_access: "gym",
 };
-localStorage.setItem("JSON1",JSON.stringify(userData))
-console.log(JSON.parse(localStorage.getItem("JSON1")))
+localStorage.setItem("JSON1", JSON.stringify(userData));
+console.log(JSON.parse(localStorage.getItem("JSON1")));
 
 const genAI = new GoogleGenerativeAI("AIzaSyAWqTZzqkPG9VlZvn5AwS2aeu4KoPTGLPk");
-const generationConfig = {	
-    temperature: 2,
-	topK: 1,
-	topP: 1,
-	maxOutputTokens: 2048,
-	response_mime_type:'application/json'
+const generationConfig = {
+  temperature: 2,
+  topK: 1,
+  topP: 1,
+  maxOutputTokens: 2048,
+  response_mime_type: "application/json",
 };
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
@@ -32,7 +32,8 @@ async function generateResponse(user) {
     const prompt = `your task is to provide the asked info into a javascript object, usable out-of-the-box as an element in code.
      use clear and concise keys, suchs as days of the week and topics. 
      open and close the answer with {} and add no other element or letter before or after.
-     use under score "_" instead of "-". make sure all the object keys are of the following:"age","height,"weight","unit"
+     use under score "_" instead of "-". make sure all the object keys are of the following:
+     "age","height,"weight","unit","profile","unitSummery", "currentActivity", "routine". the routine key shoul always have "activity" and "description".
      I am a potential israeli army recruit,  and that i live in israel. saturday is a rest day and no activity is allowed.
      assume i am a student, my school begine around 8am and ends in around 2pm.
      incude a short summery about the requirements and specific mental and physical challenges that the ${user.Unit} entails.
@@ -41,20 +42,26 @@ async function generateResponse(user) {
     my army profile is ${user.Profile}. prepare a workout routine centered around prospering in my chosen unit, center it around the most
      important physical aspect of the rule. make sure the workouts provided are as explained as possible.
      ensure that in the response you have the following structure: excercise_routine:<day of the week>:decription,activity. make sure every day is referenced even if it is a rest day. `;
-    const result = await model.generateContent(prompt,generationConfig);
+    const result = await model.generateContent(prompt, generationConfig);
     console.log(result.response.text());
     return result.response.text();
   } catch (error) {
     console.error(error);
   }
 }
-const button = document.getElementById("button");
-const genAIdiv = document.getElementById("result");
+const button = document.getElementById("generatebutton");
+const unitsummerydiv = document.getElementById("unitDescription");
+const exercisediv = document.getElementById("unitexercise");
 
 button.addEventListener("click", async function () {
   const response = await generateResponse(userData);
-  console.log(response.replace("javascript", "").replace(" const", "").replaceAll("`",""));
-  let dataobject = response.replace("javascript", "").replace(" const", "").replaceAll("`","");
+  console.log(
+    response.replace("javascript", "").replace(" const", "").replaceAll("`", "")
+  );
+  let dataobject = response
+    .replace("javascript", "")
+    .replace(" const", "")
+    .replaceAll("`", "");
 
   let Userobject = await JSON.parse(dataobject);
   const responsedata = {
@@ -64,14 +71,15 @@ button.addEventListener("click", async function () {
     unit: Userobject.unit,
     currentActivity: Userobject.exercise_background,
     routine: Userobject.exercise_routine,
-    profile: Userobject.army_profile,
-    unitSummery: Userobject.unit_summary
+    profile: Userobject.profile,
+    unitSummery: Userobject.unitSummary,
   };
-  console.log(responsedata);
-  genAIdiv.textContent ="your AI generated excercis for friday are:" + responsedata.age;
-})
-  // .routine.Friday.description
 
+  console.log(Userobject);
+  unitsummerydiv.innerHTML = `${Userobject.unitSummery}`;
+  exercisediv.textContent = `${Userobject.exercise_routine}`;
+});
+// .routine.Friday.description
 
 // // generateResponse(userData);
 // run this:
