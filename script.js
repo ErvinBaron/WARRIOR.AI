ab43f992303a909c1a5a6d0e74970ca2d5ef012f
ErvinBaron <ervinbaron@gmail.com>
2024-12-04 10:16:22 +0200

    added home page adjusments

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-//Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-//Set-ExecutionPolicy Restricted -Scope CurrentUser
 
-// const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-// const genAI = new GoogleGenerativeAI("YOUR_API_KEY");
-// const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-// const prompt = "Explain how AI works";
-
-// const result = await model.generateContent(prompt);
-// console.log(result.response.text());
 
 const dropdownBtn = document.getElementById("btn");
 const dropdownMenu = document.getElementById("dropdown");
